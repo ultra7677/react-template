bad261d41ae04f80656719fa2af339b3c4da9955
ultra7677 <ultra7677@gmail.com>
2020-08-14 22:13:12 +0800

    set up prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let a = 'a';
+  const a = 'a';
   return (
     <div className="App">
       <header className="App-header">
